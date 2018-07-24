b5b0fe61faadd9c88ec890c067f37c209f7affa0
WangTsung <charlie1081@yahoo.com.tw>
2018-07-24 14:02:09 +0800

    TestCommit

---
+++ index.js
@@ -0,0 +1,72 @@
+$(function () {
+
+    $("#submitBtn").click(function () {
+        var radioBtn = $('input[name=radioBtn]:checked').val();
+
+        getData(radioBtn);
+
+    });
+
+    function getData(symbol) {
+        $.ajax({
+
+            type: 'GET',
+            url: 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + symbol + '&interval=5min&apikey=40EG681TIY1NSO94',
+            success: function (data) {
+                var temp = [
+                    []
+                ];
+                var xaxis = [];
+                var dataLength = Object.keys(data['Time Series (5min)']).length;
+
+                var i = 0;
+                for (var j = (dataLength - 1); j >= 0; j--) {
+                    var time = Object.keys(data['Time Series (5min)'])[j];
+                    if (time.substr(0, 10) != Object.keys(data['Time Series (5min)'])[0].substr(0, 10)) {
+                        continue;
+                    }
+                    xaxis[i] = time;
+                    temp[i] = [i, data['Time Series (5min)'][time]['4. close']];
+                    i++;
+                }
+
+                makeFlot(temp, xaxis);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log(xhr.responseText);
+            }
+        });
+    }
+
+    function makeFlot(data, xaxis) {
+        // console.log(data);
+
+        var dataset = [{
+            label: "line1",
+            data: data
+        }];
+
+        var options = {
+            series: {
+                lines: {
+                    show: true
+                },
+                points: {
+                    radius: 3,
+                    show: true
+                }
+            },
+
+            // xaxis: {
+            //     tickSize: [10, xaxis]
+            // }
+
+            xaxis: {
+                mode: "time"
+            }
+        };
+
+        $.plot($("#flot-placeholder"), dataset, options);
+
+    }
+});
